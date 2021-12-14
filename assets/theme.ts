a6f3f858cbf21bf01a695d12e5ce97bb0b527a4e
Luke Edwards <luke.edwards05@gmail.com>
2021-12-13 21:02:01 -0800

    chore: attach `theme.ts` script

---
+++ assets/theme.ts
@@ -0,0 +1,36 @@
+if (document.readyState !== 'loading') init();
+else addEventListener('DOMContentLoaded', init);
+
+function setter(isDark: boolean) {
+	try {
+		let theme = isDark ? 'dark' : 'light';
+		document.documentElement.setAttribute('theme', theme);
+		localStorage.theme = JSON.stringify({ theme });
+	} catch (err) {
+		// security error
+	}
+}
+
+function init() {
+	let media;
+
+	try {
+		media = window.matchMedia('(prefers-color-scheme:dark)');
+		media.onchange = ev => setter(ev.matches);
+	} catch (err) {
+		// no support
+	}
+
+	try {
+		let value = localStorage.theme;
+		let row = value && JSON.parse(value);
+
+		setter(
+			row ? /light|dark/.test(row.theme)
+			: media ? media.matches
+			: false
+		);
+	} catch (err) {
+		// security error
+	}
+}
