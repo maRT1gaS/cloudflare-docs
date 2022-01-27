9ab49b92067d9a5d97872f9c8e5e5d120964b085
Luke Edwards <luke.edwards05@gmail.com>
2022-01-26 23:23:25 -0800

    fix(timeago): use `suffix` option

--- assets/timeago.ts
+++ assets/timeago.ts
@@ -3,5 +3,5 @@ import fromnow from 'fromnow';
 export function init() {
 	let time = document.querySelector('footer time');
 	let datetime = time && time.getAttribute('datetime');
-	if (datetime) time.textContent = fromnow(datetime, { max: 1 }) + ' ago';
+	if (datetime) time.textContent = fromnow(datetime, { max: 1, suffix: true });
 }
