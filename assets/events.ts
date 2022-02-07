2599adbd553185d6ddb9ce6e6a7d6ac3650bfb56
Luke Edwards <luke.edwards05@gmail.com>
2022-02-07 12:14:45 -0800

    fix(events): keyboard accessible dropdown menu;
    - for the sidebar
    - matches existing sidebar keyboard behavior (don't agree with looping logic)

--- assets/events.ts
+++ assets/events.ts
@@ -7,50 +7,92 @@ export function mobile() {
   });
 }
 
+function $focus(elem: HTMLElement, bool: boolean) {
+  elem.toggleAttribute('is-focus-visible', bool);
+  if (bool) elem.focus();
+}
+
 // add focus attribute to activeElement if keyboard trigger
 export function focus() {
-  let attr = 'is-focus-visible';
-
   let isTAB = false;
   addEventListener('keydown', ev => {
     isTAB = ev.which === 9;
   });
 
   addEventListener('focusin', ev => {
-    if (isTAB) (ev.target as HTMLElement).setAttribute(attr, '');
+    if (isTAB) $focus(ev.target as HTMLElement, true);
   });
 
   addEventListener('focusout', ev => {
-    (ev.target as HTMLElement).removeAttribute(attr);
+    $focus(ev.target as HTMLElement, false);
   });
 }
 
 export function dropdowns() {
   let attr = 'data-expanded';
 
-  addEventListener('focusin', console.log);
-
   document.querySelectorAll('.Dropdown').forEach(div => {
     let btn = div.querySelector('button');
+    let links = div.querySelectorAll<HTMLAnchorElement>('li>a');
+    let focused = 0; // index
 
-    if (btn) {
-      let clickaway: EventListener = ev => {
+    if (btn && links.length > 0) {
+      let arrows: EventListener = (ev: KeyboardEvent) => {
+        let key = ev.which;
+        let isTAB = key === 9;
+
+        // ESCAPE ~> close
+        if (key === 27) return close(ev);
+
+        // DOWN / TAB ~> next
+        if (isTAB || key === 40) focused++;
+        // UP / SHIFT+TAB ~> prev
+        else if (key === 38 || (isTAB && ev.shiftKey)) focused--;
+
+        // loop focus around menu
+        if (focused < 0) focused = links.length;
+        else focused %= links.length;
+
+        if (isTAB) ev.preventDefault();
+        $focus(links[focused], true);
+      };
+
+      let close: EventListener = ev => {
         ev.stopPropagation();
-        removeEventListener('click', clickaway);
+        removeEventListener('click', close);
+
+        for (let i=0; i < links.length; i++) {
+          links[i].setAttribute('tabindex', '-1');
+        }
+
         div.setAttribute(attr, 'false');
         btn.setAttribute(attr, 'false');
-      }
+
+        div.removeEventListener('keydown', arrows);
+      };
+
+      let open: EventListener = ev => {
+        ev.stopPropagation();
+        addEventListener('click', close);
+
+        for (let i=0; i < links.length; i++) {
+          links[i].removeAttribute('tabindex');
+        }
+
+        div.setAttribute(attr, 'true');
+        btn.setAttribute(attr, 'true');
+
+        // focus the first link
+        $focus(links[focused=0], true);
+
+        div.addEventListener('keydown', arrows);
+      };
 
       btn.addEventListener('click', ev => {
         if (div.getAttribute(attr) === 'true') {
-          clickaway(ev);
+          close(ev);
         } else {
-          ev.stopPropagation();
-          addEventListener('click', clickaway);
-          div.setAttribute(attr, 'true');
-          btn.setAttribute(attr, 'true');
-          // TODO: focus first
-          // TODO: keyboard listeners
+          open(ev);
         }
       });
     }
